01ee768fefcfca28b719a34f97b17a99516448e0
fkrhtmq123 <fkrhtmq123@naver.com>
2020-09-01 09:44:02 +0900

    2020/09/01 Ch03 IoC 실습

---
+++ Ch03/src/main/java/kr/co/ch03/Internet.java
@@ -0,0 +1,12 @@
+package kr.co.ch03;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class Internet {
+	
+	public void access() {
+		System.out.println("인터넷 연결...");
+	}
+	
+}
